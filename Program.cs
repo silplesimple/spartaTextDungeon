f82eb7f4c3486d231b52d564ca478ba98bededa5
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-01-09 12:13:41 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace spartaTextDungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            너 내가 보이니
+        }
+    }
+}
