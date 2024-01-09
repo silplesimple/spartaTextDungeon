bf21597f9dacf3bcec4055b94dccd7a30f7ec78c
s8st <153998744+s8st@users.noreply.github.com>
2024-01-09 15:58:25 +0900

    시작 화면 기초

--- StartMenu.cs
+++ StartMenu.cs
@@ -4,6 +4,11 @@ public class Class1
 {
 	public Class1()
 	{
-		//test
+		//깃허브 테스트 
+		//시작화면 만들기
+		// commit은 내 컴퓨터에 저장하며 설명글 추가
+		// push 원격 저장소로 올리기
+		// fetch 새로고침 ---> 새로운 내용있으면 pull
+		// branch ?? 다음에 공부하기....
 	}
 }
