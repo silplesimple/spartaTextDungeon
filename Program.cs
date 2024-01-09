27a7168eb188138521e44e19b5396b36a5b2ab56
dch1114 <dch1114@naver.com>
2024-01-09 12:53:21 +0900

    test

--- Program.cs
+++ Program.cs
@@ -5,12 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-<<<<<<< Updated upstream
-            
-=======
             // 너 내가 보이니
             // test
->>>>>>> Stashed changes
+
         }
     }
 }
