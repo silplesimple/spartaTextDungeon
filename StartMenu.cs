6002945869bade2f763e1d9981a2055238be5bc7
s8st <153998744+s8st@users.noreply.github.com>
2024-01-10 19:14:01 +0900

    스타트메뉴 테스트
    테스트커밋

--- StartMenu.cs
+++ StartMenu.cs
@@ -7,11 +7,7 @@ namespace spartaTextDungeon
         static void Main(string[] args)
         {
             //깃허브 테스트 
-            //시작화면 만들기
-            // commit은 내 컴퓨터에 저장하며 설명글 추가
-            // push 원격 저장소로 올리기
-            // fetch 새로고침 ---> 새로운 내용있으면 pull
-            // branch ?? 다음에 공부하기....
+          // 하루 종일 오류나
             //아하
         }
     }
