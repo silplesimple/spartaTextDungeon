37b93f1765ea851dca74f5958bbd89ea8d8e2266
only1hay <only1hay@nate.com>
2024-01-10 20:20:05 +0900

    상태보기 프린트

--- Status.cs
+++ Status.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,33 @@ namespace spartaTextDungeon
 {
     internal class Status
     {
+        public class PlayerState
+        {
+            public int level;
+            public string name;
+            public int atkStat;
+            public int defSts;
+            public int HP;
+            public int gold;
+            //기본값은 private로 보호하고 변동될 수 있는 스텟들 구현하기
+            public void PrintState()
+            {
+                Console.WriteLine("상태보기"); //글씨 색 넣기 추가
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+                Console.WriteLine();
+                Console.WriteLine($"LV. {level}");
+            }
+        }
+       
         static void Main(string[] args)
         {
-            // 인벤토리
-           
+            PlayerState newstate=new PlayerState();
+            newstate.level = 01;
+
+
+            newstate.PrintState();
+
         }
+
     }
 }
