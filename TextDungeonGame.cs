45af6e0c553990bb369cc6de0428badd6a661ae0
dch1114 <dch1114@naver.com>
2024-01-12 12:22:04 +0900

    병합

--- TextDungeonGame.cs
+++ TextDungeonGame.cs
@@ -257,7 +257,7 @@ namespace spartaTextDungeon
         {
             Console.Clear();
             Console.WriteLine($"◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆");
-            //Console.WriteLine("{0}",("◆")*15);
+            // Console.WriteLine("{0}",("◆")*15);
             Console.WriteLine($"스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine($"이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
             Console.WriteLine($"◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆");
