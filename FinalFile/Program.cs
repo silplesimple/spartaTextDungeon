5e3242aa309f9e3cff9680ab3cff5adce3a24e6d
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-01-16 15:24:27 +0900

    직업별 스킬 구현 완료

--- FinalFile/Program.cs
+++ FinalFile/Program.cs
@@ -45,23 +45,19 @@ namespace spartaTextDungeon.finalFile
             Console.WriteLine($"◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆");
             Console.WriteLine($"원하시는 직업을 선택해주세요");
             Console.WriteLine($"1.초보자(스탯이 일정함)");
-            Console.WriteLine($"2.전사(체력이 높지만 공격력이 낮음)");
-            Console.WriteLine($"3.도적(체력이 낮지만 공격력이 높음");
-            Console.WriteLine($"4.마법사(체력이낮지만 마나가 높음)");
+            Console.WriteLine($"2.전사(체력이 높지만 공격력이 낮음)");            
+            Console.WriteLine($"3.마법사(체력이낮지만 마나가 높음)");
             Console.WriteLine($"◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆");
             //초보자
-            switch (CheckVailedInput(1, 4))
+            switch (CheckVailedInput(1, 3))
             {
                 case 1:
-                    _player = new Player("이름", "초보자", 100,50, 100,50, 10, 5, 1500, 1);
+                    _player = new Player("이름", "초보자", 100,50, 100,50, 15, 5, 1500, 1);
                     break;
                 case 2:
                     _player = new Player("이름", "전사", 250,40, 250,40, 8, 5, 1500, 1);
-                    break;
+                    break;               
                 case 3:
-                    _player = new Player("이름", "도적", 75,80, 75, 80,15, 5, 1500, 1);
-                    break;
-                case 4:
                     _player = new Player("이름", "마법사", 80, 120,80, 120, 10, 5, 1500, 1);
                     break;
             }          
@@ -153,7 +149,7 @@ namespace spartaTextDungeon.finalFile
                 Console.WriteLine("1.알파 스트라이크 - MP 10");
                 Console.WriteLine("  공격력 * 2로 하나의 적을 공격합니다");
                 Console.WriteLine("2.더블 스트라이크 - MP 15");
-                Console.WriteLine("  공격력 * 1.5로 2명의 적을 랜덤으로 공격합니다");
+                Console.WriteLine("  공격력 * 1.5로 적을 랜덤으로 2번 공격합니다");
                 Console.WriteLine("0.취소\n");
                 Console.WriteLine("원하시는 행동을 입력해주세요\n>>");
                 switch (CheckVailedInput(0, 2))
@@ -165,10 +161,253 @@ namespace spartaTextDungeon.finalFile
                         AlphaStrike();
                         break;
                     case 2:
+                        DoubleStrike();
                         break;
-                    
                 }
 
+            }
+            else if(_player.Class=="마법사")
+            {
+                Console.WriteLine("1.파이어볼 - MP 20");
+                Console.WriteLine("  공격력 * 3로 하나의 적을 공격합니다");
+                Console.WriteLine("2.아이스 스피어 - MP 30");
+                Console.WriteLine("  공격력 * 2로 적을 랜덤으로 2번 공격합니다");
+                Console.WriteLine("3.메테오 - MP 80");
+                Console.WriteLine("  공격력 * 2로 적을 랜덤으로 10번 공격합니다");
+                Console.WriteLine("0.취소\n");
+                Console.WriteLine("원하시는 행동을 입력해주세요\n>>");
+                switch (CheckVailedInput(0, 3))
+                {
+                    case 0:
+                        Battle();
+                        break;
+                    case 1:
+                        FireBall();
+                        break;
+                    case 2:
+                        IceSpear();
+                        break;
+                    case 3:
+                        Meteor();
+                        break;
+                }
+            }
+            else if(_player.Class=="초보자")
+            {
+                Console.WriteLine("초보자는 스킬이 없습니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Battle();
+                        break;         
+                }
+            }
+        }
+
+        private static void FireBall()
+        {
+            Console.Clear();
+            for (int i = 0; i < _monsters.Count; i++)
+            {
+                if (_monsters[i].IsDead())
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"{i + 1}. {_monsters[i]}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine($"{i + 1}. {_monsters[i]}");
+                }
+            }
+            Console.WriteLine("맞출 대상을 선택해주세요");
+            Console.WriteLine("0.취소");
+            int choice = CheckVailedInput(0, _monsters.Count);
+            if (choice == 0)
+            {
+                Skill();
+            }
+            else
+            {
+                if (_monsters[choice - 1].IsDead())
+                {
+                    Console.Clear();
+                    Console.WriteLine("잘못된 입력입니다. 이미 죽은 몬스터를 선택할 수 없습니다.");
+                    Console.WriteLine("\n0 뒤로");
+                    switch (CheckVailedInput(0, 0))
+                    {
+                        case 0:
+                            Skill();
+                            break;
+                    }
+                }
+                else
+                {
+                    FireBallAttack(choice);
+                }
+            }
+        }
+
+        private static void FireBallAttack(int choiceIndex)
+        {
+            Console.Clear();
+            int fireBallMp = 20;
+            int fireBall = _player.Attack * 3;
+            if (_player.MP >= fireBallMp)
+            {
+                _player.MP -= fireBallMp;
+                _monsters[choiceIndex - 1].HP -= fireBall;
+                Console.WriteLine($"{_player.Name}의 스킬 발동!");
+                Console.WriteLine($"알파 스트라이크!");
+                Console.WriteLine($"\n{_monsters[choiceIndex - 1].Name}의 HP가 {fireBall}만큼 닳았습니다! ");
+                if (_monsters[choiceIndex - 1].IsDead())
+                {
+                    Console.WriteLine($"몬스터 {_monsters[choiceIndex - 1].Name}을(를) 처치했습니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("마나가 부족합니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Skill();
+                        break;
+                }
+            }
+            Console.WriteLine("\n0.다음");
+            switch (CheckVailedInput(0, 0))
+            {
+                case 0:
+                    EnemyPhase();
+                    break;
+            }
+        }
+
+        private static void IceSpear()
+        {
+            Console.Clear();
+            int iceSpearMp = 30;
+            int iceSpear = _player.Attack * 2;
+            Random random = new Random();
+            if (_player.MP >= iceSpearMp)
+            {
+                _player.MP -= iceSpearMp;
+                Console.WriteLine($"{_player.Name}의 스킬 발동!");
+                Console.WriteLine($"아이스 스피어!");
+                for (int i = 0; i < 2; i++)
+                {
+                    int randomIndex = random.Next(0, _monsters.Count);
+                    _monsters[randomIndex].HP -= iceSpear;
+                    Console.WriteLine($"\n{_monsters[randomIndex].Name}의 HP가 {iceSpear}만큼 닳았습니다! ");
+                    if (_monsters[randomIndex].IsDead())
+                    {
+                        Console.WriteLine($"몬스터 {_monsters[randomIndex].Name}을(를) 처치했습니다.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("마나가 부족합니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Skill();
+                        break;
+                }
+            }
+            Console.WriteLine("\n0.다음");
+            switch (CheckVailedInput(0, 0))
+            {
+                case 0:
+                    EnemyPhase();
+                    break;
+            }
+        }
+
+        private static void Meteor()
+        {
+            Console.Clear();
+            int meteorMp = 80;
+            int meteor = _player.Attack * 2;
+            Random random = new Random();
+            if (_player.MP >= meteorMp)
+            {
+                _player.MP -= meteorMp;
+                Console.WriteLine($"{_player.Name}의 스킬 발동!");
+                Console.WriteLine($"메테오!!!!!");
+                for (int i = 0; i < 10; i++)
+                {
+                    int randomIndex = random.Next(0, _monsters.Count);
+                    _monsters[randomIndex].HP -= meteor;
+                    Console.WriteLine($"\n{_monsters[randomIndex].Name}의 HP가 {meteor}만큼 닳았습니다! ");
+                    if (_monsters[randomIndex].IsDead())
+                    {
+                        Console.WriteLine($"몬스터 {_monsters[randomIndex].Name}을(를) 처치했습니다.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("마나가 부족합니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Skill();
+                        break;
+                }
+            }
+            Console.WriteLine("\n0.다음");
+            switch (CheckVailedInput(0, 0))
+            {
+                case 0:
+                    EnemyPhase();
+                    break;
+            }
+        }        
+        private static void DoubleStrike()
+        {
+            Console.Clear();
+            int doubleMp = 15;
+            int doubleStrike = _player.Attack * 3/2;
+            Random random = new Random();
+            if (_player.MP >= doubleMp)
+            {
+                _player.MP -= doubleMp;                
+                Console.WriteLine($"{_player.Name}의 스킬 발동!");
+                Console.WriteLine($"더블 스트라이크!");
+                for(int i=0;i<2;i++)
+                {                    
+                    int randomIndex=random.Next(0, _monsters.Count);
+                    _monsters[randomIndex].HP -= doubleStrike;
+                    Console.WriteLine($"\n{_monsters[randomIndex].Name}의 HP가 {doubleStrike}만큼 닳았습니다! ");
+                    if (_monsters[randomIndex].IsDead())
+                    {
+                        Console.WriteLine($"몬스터 {_monsters[randomIndex].Name}을(를) 처치했습니다.");
+                    }
+                }                          
+            }
+            else
+            {
+                Console.WriteLine("마나가 부족합니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Skill();
+                        break;
+                }
+            }
+            Console.WriteLine("\n0.다음");
+            switch (CheckVailedInput(0, 0))
+            {
+                case 0:
+                    EnemyPhase();
+                    break;
             }
         }
 
@@ -197,7 +436,22 @@ namespace spartaTextDungeon.finalFile
             }
             else
             {
-                AlphaStrikeAttack(choice);
+                if (_monsters[choice-1].IsDead())
+                {                    
+                    Console.Clear();
+                    Console.WriteLine("잘못된 입력입니다. 이미 죽은 몬스터를 선택할 수 없습니다.");
+                    Console.WriteLine("\n0 뒤로");
+                    switch (CheckVailedInput(0, 0))
+                    {
+                        case 0:
+                            Skill();
+                            break;
+                    }
+                }
+                else
+                {
+                    AlphaStrikeAttack(choice);
+                }
             }            
         }
         private static void AlphaStrikeAttack(int choiceIndex)
@@ -205,14 +459,28 @@ namespace spartaTextDungeon.finalFile
             Console.Clear();
             int alphaMp = 10;
             int alphaStrike = _player.Attack * 2;
-            _player.MP-=alphaMp;
-            _monsters[choiceIndex - 1].HP -= alphaStrike;
-            Console.WriteLine($"{_player.Name}의 스킬 발동!");
-            Console.WriteLine($"알파 스트라이크!");
-            Console.WriteLine($"\n{_monsters[choiceIndex - 1].Name}의 HP가 {alphaStrike}만큼 닳았습니다! ");
-            if (_monsters[choiceIndex - 1].IsDead())
+            if (_player.MP >= alphaMp)
             {
-                Console.WriteLine($"몬스터 {_monsters[choiceIndex - 1].Name}을(를) 처치했습니다.");
+                _player.MP -= alphaMp;
+                _monsters[choiceIndex - 1].HP -= alphaStrike;
+                Console.WriteLine($"{_player.Name}의 스킬 발동!");
+                Console.WriteLine($"알파 스트라이크!");
+                Console.WriteLine($"\n{_monsters[choiceIndex - 1].Name}의 HP가 {alphaStrike}만큼 닳았습니다! ");
+                if (_monsters[choiceIndex - 1].IsDead())
+                {
+                    Console.WriteLine($"몬스터 {_monsters[choiceIndex - 1].Name}을(를) 처치했습니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("마나가 부족합니다");
+                Console.WriteLine("\n0.다음");
+                switch (CheckVailedInput(0, 0))
+                {
+                    case 0:
+                        Skill();
+                        break;
+                }
             }
             Console.WriteLine("\n0.다음");
             switch(CheckVailedInput(0,0))
